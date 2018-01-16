2d534e152d395f0531004d4961a8296c17a800f1
franklee1 <34935403+franklee1@users.noreply.github.com>
2018-01-16 02:46:18 -0800

    Add files via upload

---
+++ src/drafted/governance-categories.h
@@ -0,0 +1,58 @@
+
+/*
+	CATEGORY MAPPING
+
+	* means the category has an associated class
+	
+	CmasterNetwork: master NETWORK (ROOT)
+	has:
+		vector<CNetworkVariable> vecNetworkVariables;
+		vector<CmasterProject> vecProjects;
+		vector<CGovernanceObject> vecProposals;
+		vector<CBudgetContract> vecContracts;
+		vector<CBudgetUsers> vecUsers;
+
+	CmasterProject:
+	has:
+		name
+		employees
+
+*/
+
+	// master NETWORK (ROOT)
+	// 	-> NETWORK VARIABLE
+	// 		-> switch, setting
+	// 	-> CATEGORIES
+	// 		-> LEVEL
+	// 			-> I, II, III, IV, V, VI, VII, VIII, IX, X, XI
+	// 		-> VALUEOVERRIDE
+	// 			-> NETWORK, OWNER 
+	// 		-> PROJECT*
+	// 			-> TYPES
+	// 				-> SOFTWARE
+	// 					-> CORE, NONCORE
+	// 				-> HARDWARE
+	// 				-> PR
+	// 			-> PROJECT REPORT*
+	// 				-> UPDATE
+	// 			-> PROJECT MILESTONE*
+	// 				-> START, ONGOING, COMPLETE, FAILURE
+	// 			-> PROPOSAL*
+	// 				-> FUNDING, GOVERNANCE, AMEND, GENERIC
+	// 			-> CONTRACT*
+	// 				-> TYPE
+	// 					-> INTERNAL, EXTERNAL
+	// 				-> STATUS
+	// 					-> OK
+	// 	-> GROUPS
+	// 		-> GROUP1
+	// 			-> USER1 (only users are allowed here in this scope)
+	// 			-> USER2
+	// 		-> GROUP2 (EVO)
+	// 			-> VALUEOVERRIDE (STORE=masterDRIVE)
+	// 			-> USER1
+
+	// 	-> COMPANIES
+	// 		-> COMPANY1
+	// 		-> DAO1
+
